70e76500b7c57e4af62988a74444660a90a06b53
McLoy <tkachenko.vlad@hotmail.com>
2016-09-05 00:54:39 +0300

    Initial commit

---
+++ src/ObjectAnalyzerTest.java
@@ -0,0 +1,67 @@
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+public class ObjectAnalyzerTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> squares = new ArrayList<Integer>();
+        for (int i = 0; i <=5 ; i++) squares.add(i*i);
+            System.out.println(new ObjectAnalyzer().toString(squares));
+        }
+}
+
+class ObjectAnalyzer{
+        /**
+         * Преобразует объект в строку,
+         * в которой перечислены все поля
+         * @param obj Объект
+         * @return Строка, в которой указаны имя объекта,
+         * имена всех полей и их значения
+         */
+
+        public String toString(Object obj){
+            if (obj == null) return "null";
+            if (visited.contains(obj)) return "...";
+            visited.add(obj);
+            Class cl = obj.getClass();
+            if (cl == String.class) return (String)obj;
+            if (cl.isArray()){
+                String r = cl.getComponentType() + "[]{";
+                for (int i = 0; i < Array.getLength(obj); i++) {
+                    if (i > 0) r += ",";
+                    Object val = Array.get(obj, i);
+                    if (cl.getComponentType().isPrimitive()) r += val;
+                    else r += toString(val);
+                }
+                return r + "}";
+            }
+
+            String r = cl.getName();
+            do {
+                r += "[";
+                Field[] fields = cl.getDeclaredFields();
+                AccessibleObject.setAccessible(fields, true);
+                for (Field f : fields){
+                    if (!Modifier.isStatic(f.getModifiers())) {
+                        if (!r.endsWith("[")) r += ",";
+                        r += f.getName() + "=";
+                        try {
+                            Class t = f.getType();
+                            Object val = f.get(obj);
+                            if (t.isPrimitive()) r += val;
+                            else r += toString(val);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                    r += "]";
+                    cl = cl.getSuperclass();
+            }
+                while (cl != null);
+                return r;
+
+    }
+        private ArrayList<Object> visited = new ArrayList<Object>();
+}
